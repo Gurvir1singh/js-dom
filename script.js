05f67f86d51de717544a1f8ff0908ea36fde4a02
Gurvir Singh <try.gurveer+github1@gmail.com>
2025-11-19 20:06:30 +0530

    make a simple counter with js dom

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-let form = document.querySelector("form");
-let inputs = document.querySelectorAll("input");
-let cardContainer = document.querySelector(".card-container");
+let dec = document.querySelector("#decrease")
+let inc = document.querySelector("#increase")
+let value = document.querySelector(".value")
 
-form.addEventListener("submit", function (el) {
-  el.preventDefault();
-  let arr = [];
-  inputs.forEach((inp) => {
-    if (inp.type !== "submit") {
-      arr.push([inp.value]);
-    }
-  });
+let rval = Number(value.textContent)
 
-  let card = document.createElement("div");
-  card.classList.add("card");
+inc.addEventListener('click', function(){
 
-  let picture = document.createElement("div");
-  picture.classList.add("picture");
+  rval++
+  console.log(rval);
+    
+  value.innerHTML = rval;
+})
+dec.addEventListener('click', function(){
+ rval--
+  console.log(rval);
+  value.innerHTML = rval;
+})
 
-  card.appendChild(picture);
-  console.log(card);
 
-  let img = document.createElement("img")
-  img.setAttribute("src", arr[0]);
-  picture.appendChild(img);
-
-  let Uname = document.createElement("h2");
-  Uname.innerHTML = arr[1];
-  card.appendChild(Uname);
-
-  let h4 = document.createElement("h4");
-  h4.innerHTML = arr[2];
-  card.appendChild(h4);
-
-  let h5 = document.createElement("h5");
-  h5.innerHTML = arr[3];
-  card.appendChild(h5);
-
-  cardContainer.appendChild(card);
-});
