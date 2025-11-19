92d807a453f6a416d5171156b01dedc1398c6d4a
Gurvir Singh <try.gurveer+github1@gmail.com>
2025-11-20 01:19:18 +0530

    h1 generator

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-let dec = document.querySelector("#decrease")
-let inc = document.querySelector("#increase")
-let value = document.querySelector(".value")
-
-let rval = Number(value.textContent)
-
-inc.addEventListener('click', function(){
-
-  rval++
-  console.log(rval);
-    
-  value.innerHTML = rval;
-})
-dec.addEventListener('click', function(){
- rval--
-  console.log(rval);
-  value.innerHTML = rval;
-})
-
-
+let button = document.querySelector("button");
+let main = document.querySelector("main");
+let h1
+button.addEventListener("click", function () {
+  h1 = document.createElement("h1");
+  h1.innerHTML= "we are grow bro"
+let top = Math.random() * 100;
+let left = Math.random() * 100;
+let rotate = Math.random() * 360;
+  h1.style.fontSize = '80px';
+  h1.style.position = 'absolute';
+  h1.style.top = `calc(${top}%)`
+  h1.style.left = `${left}%`
+  h1.style.transform = `rotate(${rotate}deg)`
+  h1.style.zIndex = `-1`
+  main.appendChild(h1);
+  console.log("hello");
+  
+});
