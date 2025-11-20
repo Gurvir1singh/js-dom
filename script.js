71c9e3a17c38bad90a8e5b2c23459c2c87a8dd30
Gurvir Singh <try.gurveer+github1@gmail.com>
2025-11-21 00:47:06 +0530

    colorfull pen using grid

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let body = document.querySelector("body");
-let box = document.querySelector(".box");
+let grid = document.querySelector(".grid");
+let i = 0;
+while (i < 5000) {
+  let box = document.createElement("div");
+  box.classList.add("box");
+  grid.appendChild(box);
+  i++;
+}
 
-body.addEventListener("mousemove", function (mouse) {
-  console.log(mouse.x);
-  console.log(mouse.y);
-
-  box.style.top = `${mouse.y -20}px`;
-  box.style.left = `${mouse.x}px`;
+let box = document.querySelectorAll(".box");
+box.forEach(function (elem) {
+  let c1 = Math.random() * 256;
+  let c2 = Math.random() * 256;
+  let c3 = Math.random() * 256;
+  elem.addEventListener("mouseenter", function (val) {
+    elem.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
+    console.log("hello");
+  });
 });
+console.log(box);
